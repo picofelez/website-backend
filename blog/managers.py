c45896ac19b937584a3713ae9e2f5576363b3756
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2023-01-29 18:44:14 +0330

    Add: PublishedArticleManager

---
+++ blog/managers.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+
+class PublishedArticleManager(models.Manager):
+    """
+    Published shop manager.
+    """
+
+    def get_queryset(self):
+        return super(PublishedArticleManager, self).get_queryset().filter(status='p', publishe_time__lte=timezone.now)
