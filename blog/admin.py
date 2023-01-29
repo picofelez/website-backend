2e8f97057e869a7184d41ad6bc8dcab163d489cd
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2023-01-29 18:41:19 +0330

    Add: Article model

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from .models import Tag
+from .models import Tag, Article
 
 
 # Register your models here.
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     pass
+
+
+class TagArticleInline(admin.TabularInline):
+    model = Article.tags.through
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_thumbnail', 'summary', 'status')
+    list_filter = ('created',)
+    search_fields = ('title', 'summary', 'description')
+    list_editable = ('status',)
+    fieldsets = (
+        ('اطلاعات مقاله', {'fields': ('title', 'summary', 'description')}),
+        ('تصویر و نویسنده', {'fields': ('image',)}),
+        ('وضعیت مشاهده', {'fields': ('publish_time', 'status')}),
+    )
+
+    inlines = [
+        TagArticleInline
+    ]
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from extensions.utils import slug_generator
+from django.utils import timezone
+from django.utils.html import format_html
 
 
 # Create your models here.
@@ -16,3 +17,38 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Article(models.Model):
+    """
+    The Article main model,
+    Many-To-Many relationship with Tag model.
+    """
+
+    class StatusChoices(models.TextChoices):
+        published = 'a', 'منتشر شده'
+        draft = 'd', 'چک نویس'
+
+    title = models.CharField(max_length=155, verbose_name='عنوان')
+    summary = models.CharField(max_length=255, verbose_name='خلاصه')
+    description = models.TextField(verbose_name='توضیحات')
+    image = models.ImageField(upload_to='', verbose_name='تصویر اصلی')
+    tags = models.ManyToManyField(
+        Tag, related_name='articles', verbose_name='برچسب ها'
+    )
+    status = models.CharField(max_length=5, choices=StatusChoices.choices, verbose_name='وضعیت انتشار')
+    publish_time = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated = models.DateTimeField(auto_now=True, verbose_name='تاریخ ویرایش')
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = '2. مقالات'
+
+    def __str__(self):
+        return self.title
+
+    def get_thumbnail(self):
+        return format_html(f"<img width=100 height=75 style='border-radius: 5px;' src='{self.image.url}'>")
+
+    get_thumbnail.short_description = "تصویر"
