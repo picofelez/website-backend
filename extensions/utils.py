192f7c983b503a1e5e7f9576f47588328258b624
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2023-01-29 21:58:00 +0330

    Add: jalali date utils

--- extensions/utils.py
+++ extensions/utils.py
@@ -2,6 +2,7 @@ import os
 import random
 
 from django.utils import timezone
+from . import jalali
 
 
 def generate_product_id():
@@ -53,3 +54,21 @@ def upload_article_image_path(instance, filename):
 
 def slug_generator(slug) -> str:
     return slug.replace(' ', '-')
+
+
+def jalali_converter(time) -> str:
+    jmonths = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد',
+               'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند']
+
+    time_to_str = f'{time.year},{time.month},{time.day}'
+    time_to_tuple = jalali.Gregorian(time_to_str).persian_tuple()
+    time_to_list = list(time_to_tuple)
+
+    for index, month in enumerate(jmonths):
+        if time_to_list[1] == index + 1:
+            time_to_list[1] = month
+            break
+
+    output = f'{time_to_list[2]} {time_to_list[1]} {time_to_list[0]} '
+
+    return output
