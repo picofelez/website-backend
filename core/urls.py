94d4fc19029a0682be914d70f994be52a1695d83
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2023-01-30 19:03:15 +0330

    Add: AboutUs template view

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import (
-    Home
+    Home,
+    AboutUsView
 )
 
 app_name = 'core'
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('about-us/', AboutUsView.as_view(), name='about-us'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 
 
@@ -9,3 +10,10 @@ class Home(TemplateView):
     index view.
     """
     template_name = 'core/home.html'
+
+
+class AboutUsView(TemplateView):
+    """
+    About us view.
+    """
+    template_name = 'core/about-us.html'
