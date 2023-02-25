18a3363268334fe8753356bff74d7058b8f3c15a
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2023-02-25 19:40:34 +0330

    Update: ShopPanelAccess mixin

--- shop/mixins.py
+++ shop/mixins.py
@@ -3,6 +3,6 @@ from django.http import Http404
 
 class ShopPanelAccessMixin:
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user.shops.first():
+        if request.user.is_authenticated and self.get_object() in request.user.shops.all():
             return super().dispatch(request, *args, **kwargs)
         raise Http404
