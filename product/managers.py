12a9041f102d703b84336b48100a5af72a1f245b
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2023-02-25 19:00:12 +0330

    refactor: add shop__status to PublishedProductManager

--- product/managers.py
+++ product/managers.py
@@ -5,5 +5,8 @@ class PublishedProductsManager(models.Manager):
     """
     published product manager.
     """
+
     def get_queryset(self):
-        return super(PublishedProductsManager, self).get_queryset().filter(is_confirmed=True, is_active=True)
+        return super(PublishedProductsManager, self).get_queryset().filter(
+            is_confirmed=True, is_active=True, shop__status='a'
+        )
