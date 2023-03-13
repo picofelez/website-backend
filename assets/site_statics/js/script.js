f04ee0e533ec1f97eb6a8420bdf3049088266b56
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2023-03-13 17:58:07 +0330

    Update: purchase steps

--- assets/site_statics/js/script.js
+++ assets/site_statics/js/script.js
@@ -1,53 +1,53 @@
-$(document).ready(function () {
-    function countDown(duration, display) {
-        var start = Date.now(),
-            diff,
-            minutes,
-            seconds;
+function countDown(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
 
-        function timer() {
-            // get the number of seconds that have elapsed since
-            // startTimer() was called
-            diff = duration - (((Date.now() - start) / 1000) | 0);
+    function timer() {
+        // get the number of seconds that have elapsed since
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
 
-            // does the same job as parseInt truncates the float
-            minutes = (diff / 60) | 0;
-            seconds = (diff % 60) | 0;
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            display.textContent = minutes + ":" + seconds;
+        display.textContent = minutes + ":" + seconds;
 
-            if (diff <= 0) {
-                // add one second so that the count down starts at the full duration
-                // example 05:00 not 04:59
-                start = Date.now() + 1000;
-            }
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
         }
-
-        // we don't want to wait a full second before the timer starts
-        timer();
-        setInterval(timer, 1000);
     }
+
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
 
 // get csrf-token
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== "") {
-            const cookies = document.cookie.split(";");
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + "=")) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
         }
-        return cookieValue;
     }
+    return cookieValue;
+}
 
+$(document).ready(function () {
 //    mobile nav toggle
     $('#main-nav-btn').click(function () {
         $('#mobile-main-nav').toggle(500).css('display', 'flex')
