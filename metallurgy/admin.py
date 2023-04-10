e4eda684ade04bdae675ef3d0cc22e80d48dba61
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-04-10 19:35:49 +0330

    Add: WorkSample & WorkSampleImage model

--- metallurgy/admin.py
+++ metallurgy/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import WorkSample, WorkSampleImage
+
 
 # Register your models here.
+class WorkSampleImageInline(admin.TabularInline):
+    model = WorkSampleImage
+    extra = 0
+
+
+@admin.register(WorkSample)
+class WorkSampleAdmin(admin.ModelAdmin):
+    inlines = [
+        WorkSampleImageInline
+    ]
+
+
+@admin.register(WorkSampleImage)
+class WorkSampleImage(admin.ModelAdmin):
+    pass
--- metallurgy/models.py
+++ metallurgy/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class WorkSample(models.Model):
+    class StatusChoices(models.TextChoices):
+        draft = 'd', 'چک نویس'
+        published = 'p', 'منتشر شده'
+
+    title = models.CharField(max_length=255, verbose_name='عنوان')
+    description = models.TextField(verbose_name='توضیحات')
+    image = models.ImageField(upload_to='portfolio/', verbose_name='تصویر')
+    status = models.CharField(max_length=10, choices=StatusChoices.choices, verbose_name='وضعیت')
+    publish_time = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
+    address = models.CharField(max_length=75, null=True, blank=True, verbose_name='مکان')
+    date = models.CharField(max_length=75, null=True, blank=True, verbose_name='تاریخ اجرا')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+
+    class Meta:
+        verbose_name = 'نمونه کار'
+        verbose_name_plural = '1. نمونه کار ها'
+
+    def get_name_replace(self):
+        return f"{self.title.replace(' ', '-')}"
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class WorkSampleImage(models.Model):
+    work_sample = models.ForeignKey(
+        WorkSample, on_delete=models.CASCADE, related_name='images', verbose_name='نمونه کار'
+    )
+    image = models.ImageField(upload_to='portfolio/', verbose_name='تصویر')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+
+    class Meta:
+        verbose_name = 'تصویر نمونه کار'
+        verbose_name_plural = '2. تصاویر نمونه کار'
