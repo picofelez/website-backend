fb8c251bf878edef4834b70e63723b436bf66653
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-06-27 21:55:01 +0330

    refactor CustomerInvoiceAccessMixin: check invoice accessibility status in mixin

--- metallurgy/mixins.py
+++ metallurgy/mixins.py
@@ -17,9 +17,15 @@ class CustomerProjectAccessMixin:
 class CustomerInvoiceAccessMixin:
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and (
-                request.user in self.get_object().project.customers.all() or request.user.is_staff
+                (
+                        request.user in self.get_object().project.customers.all() and
+                        self.get_object().accessibility_status == 'c'
+                )
+                or request.user.is_staff
         ):
             return super().dispatch(request, *args, **kwargs)
+        elif request.user.is_authenticated and self.get_object().accessibility_status == 'p':
+            raise Http404
         return redirect(
             f"{reverse('account:otp-login')}?next={reverse('metallurgy:customer-invoice-detail', args=[self.get_object().id])}"
         )
