44dc84e6f916d041fb296822c19e20c6ed8fecbf
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-07-05 16:04:14 +0330

    refactor: ShopInvoice get_weekly_records queryset

--- shop/managers.py
+++ shop/managers.py
@@ -18,16 +18,21 @@ class ActiveShopManager(models.Manager):
 class ShopInvoiceManager(models.Manager):
     def get_weekly_records(self, shop):
         one_week_ago = datetime.now() - timedelta(days=7)
-        conditions = Q()
         records = []
         days = []
         for i in range(8):
             day = one_week_ago + timedelta(days=i)
-            conditions |= Q(
-                created__date=day.date(), invoice_shop='sales', shop=shop
-            )
             days.append(jalali_converter(day.date()))
-            query = self.select_related(None).filter(conditions)
-            records.append(sum(record.get_total_invoice_price() for record in query))
+
+            qs = self.select_related(None).filter(
+                created__year=day.year,
+                created__month=day.month,
+                created__day=day.day,
+                invoice_shop='sales',
+                shop=shop
+            ).annotate(
+                day=ExtractDay('created'),
+            ).order_by('day')
+            records.append(sum(record.get_total_invoice_price() for record in qs))
 
         return [days, records]
