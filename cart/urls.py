49103a0402dfaff43418589c65c5131e5922400f
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-07-09 21:16:28 +0330

    refactor: cart button & cart urls

--- cart/urls.py
+++ cart/urls.py
@@ -8,8 +8,8 @@ from .views import (
 
 app_name = 'cart'
 urlpatterns = [
-    path('', CartListView.as_view(), name='cart-list'),
-    path('checkout/', checkout_view, name='cart-checkout'),
+    # path('', CartListView.as_view(), name='cart-list'),
+    # path('checkout/', checkout_view, name='cart-checkout'),
 
     path('add/<str:product_id>', cart_add_view, name='cart-add'),
     path('remove/<str:product_id>', cart_remove_view, name='cart-remove'),
