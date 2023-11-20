4dfcb64a0b4ba695b21b4b0b20902f78d85088df
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-11-20 16:24:36 +0330

    Update: login view, send otp code to any phone number

--- account/views.py
+++ account/views.py
@@ -35,13 +35,8 @@ def login_view(request):
 
     if form.is_valid():
         phone_number = form.cleaned_data.get('phone_number')
-        user_exist: bool = User.objects.filter(username=phone_number).exists()
-
-        if not user_exist:
-            messages.error(request, 'شماره یافت نشد، لطفا روی گزینه ثبت نام کلیک کنید.')
-        else:
-            send_otp(request, phone_number)
-            return redirect('account:otp-verify')
+        send_otp(request, phone_number)
+        return redirect('account:otp-verify')
 
     context = {
         'form': form
