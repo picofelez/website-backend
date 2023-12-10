a88f43cec587fec3bae412fce0c71cbf7e6c8e64
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-12-10 17:23:10 +0330

    Update: breadcrumbs text

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('about-us/', AboutUsView.as_view(), name='about-us'),
     path('contact-us/', CreateContactView.as_view(), name='contact-us'),
-    path('faq/', QuestionListView.as_view(), name='faq'),
+    # path('faq/', QuestionListView.as_view(), name='faq'),
     path('calculator/', IronCalculatorTemplateView.as_view(), name='calculator'),
 ]
